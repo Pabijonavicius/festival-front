779aee69e64435d5d0d3105ea5581ce4f95af4f7
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-11 19:28:40 +0300

    Fixed gallery's photos

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let hamburger = document.querySelector('#hamburger');
 let navElements = document.getElementsByClassName('navbar__items__item');
-let brandingAfter = document.querySelector('.navbar__branding-item:after');
 
 hamburger.addEventListener('click', () => {
   for (let i = 0; i < navElements.length; i++) {
